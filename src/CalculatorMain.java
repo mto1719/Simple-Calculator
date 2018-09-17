4671cc22670776c7723e1383ebce2c71c382632e
mto1719 <42977020+mto1719@users.noreply.github.com>
2018-09-17 13:40:03 -0400

    Add files via upload

---
+++ src/CalculatorMain.java
@@ -0,0 +1,11 @@
+
+public class CalculatorMain 
+{
+	public static void main(String[] args) {
+		// This is how we create an object
+		// now --> java keyword which creates an object
+		Calculator calc = new Calculator();
+		calc.add(2,6)
+		System.out.print(calc.add(2,6));
+	}
+}
