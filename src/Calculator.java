46e7437f6dcdb80a6537c6083e447537d87260c6
ThePC <ThePC@DESKTOP-3MTRAIS>
2018-09-18 01:25:14 -0400

    new

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,28 @@
-
+/**
+ * 
+ * @author Matthew To
+ *
+ */
 public class Calculator
 {
 	public int add(int num1, int num2) 
 	{
 		int ans = num1 + num2;
 		return ans;
-		
+	}	
+	int sub(int num1, int num2) 
+	{
+		int ans = num1 - num2;
+		return ans;	
 	}
-
+	int mul(int num1, int num2)
+	{
+		int ans = num1 * num2;
+		return ans;
+	}
+	int div(int num1, int num2)
+	{
+		int ans = num1 / num2;
+		return ans;
+	}
 }
--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -1,11 +1,17 @@
-
+/**
+ * 
+ * @author Matthew To
+ *
+ */
 public class CalculatorMain 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// This is how we create an object
 		// now --> java keyword which creates an object
 		Calculator calc = new Calculator();
-		calc.add(2,6)
-		System.out.print(calc.add(2,6));
+		calc.sub(4, 2);
+		System.out.print(calc.sub(4, 2));
 	}
 }
+	
